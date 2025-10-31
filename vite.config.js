dc164fa0f3d981a4b9af378b0c3038699191e212
Joha <carlajoha.all@gmail.com>
2025-10-31 09:52:29 -0400

    configurar para githubpages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-const isGitHubPages = process.env.GITHUB_PAGES === 'true'
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+
+if (isGitHubPages) {
+  copyFileSync(resolve(__dirname, '404.html'), resolve(__dirname, 'dist/404.html'))
+};
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: isGitHubPages ? '/reactjs-conference-ticket-generator/' : '/',
-})
+});
